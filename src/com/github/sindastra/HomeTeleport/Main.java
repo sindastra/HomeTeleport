e108d5c828305a87f66b9ab73545c451d9286927
Sindastra <sindastra+github@gmail.com>
2014-11-05 06:32:16 +0100

    First commit

---
+++ src/com/github/sindastra/HomeTeleport/Main.java
@@ -0,0 +1,62 @@
+/**
+ *  HomeTeleport
+ *  Copyright (C) 2014 Sindastra. All rights reserved.
+ *  https://github.com/sindastra/HomeTeleport
+ * 
+ * HomeTeleport is a simple Bukkit plugin to teleport a player home to their bed.
+ * 
+ * The above copyright notice and this notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * @author Sindastra <sindastra+github@gmail.com>
+ * @copyright (C) 2014 Sindastra. All rights reserved.
+ */
+
+package com.github.sindastra.HomeTeleport;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("Enabled.");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("Disabled.");
+	}
+	
+	@Override
+	public boolean onCommand( CommandSender sender , Command cmd , String label , String[] args ) {
+		if( cmd.getName().equalsIgnoreCase("home") ) {
+			if( !( sender instanceof Player ) )
+				sender.sendMessage("This command must be run by a player.");
+			else {
+				Player player = (Player) sender;
+				Location bedLoc = (Location) player.getBedSpawnLocation();
+				if( bedLoc != null )
+					player.teleport(bedLoc);
+				else
+					player.sendMessage( ChatColor.RED + "No bed found!" );
+			}
+			return true;
+		}		
+		return false;
+	}
+
+}
